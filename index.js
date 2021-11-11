a09cbc50580d2833bd92890540ed3382084e87c3
sanjit43 <sanjitsarker257@gmail.com>
2021-11-11 11:14:13 +0600

    gitigonre file

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ app.use(express.json())
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.0bfuq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
+//Client connected
 async function run() {
     try {
         await client.connect()
