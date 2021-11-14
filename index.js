f32fa2e8719c971e7bf7ea8872fc48f5e096ac42
sanjit43 <sanjitsarker257@gmail.com>
2021-11-14 19:00:26 +0600

    purchase and id

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const { MongoClient } = require('mongodb');
+const ObjectId = require("mongodb").ObjectId;
 require('dotenv').config()
 const cors = require('cors')
 const port = process.env.PORT || 5000;
@@ -33,17 +34,28 @@ async function run() {
             res.json(result)
         })
 
+
+
+
         //GET Orders Api
         app.get('/orders', async (req, res) => {
             const cursor = ordersCollection.find({})
-            const result = await cursor.toArray();
-            res.send(result)
+            const orders = await cursor.toArray()
+            res.json(orders)
         })
+
         //GET API
         app.get('/products', async (req, res) => {
             const cursor = productCollection.find({})
             const result = await cursor.toArray();
             res.send(result)
+        })
+
+        app.get('/products/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: ObjectId(id) };
+            const result = await productCollection.findOne(query)
+            res.json(result)
         })
     }
     finally {
