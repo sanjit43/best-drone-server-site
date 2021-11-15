8e4f10afaced03f4634e422679c3a23744703cad
sanjit43 <sanjitsarker257@gmail.com>
2021-11-15 18:50:46 +0600

    upsert action done

--- index.js
+++ index.js
@@ -35,6 +35,15 @@ async function run() {
             res.json(result);
         })
 
+        app.put('/users', async (req, res) => {
+            const user = req.body;
+            const filter = { email: user.email }
+            const options = { upsert: true };
+            const updateDoc = { $set: user }
+            const result = await userCollection.updateOne(filter, updateDoc, options);
+            res.json(result)
+        })
+
         //GET USERS API
         app.get('/users', async (req, res) => {
             const cursor = userCollection.find({});
